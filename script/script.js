20f7c587093f3ed6fe63dbc99773c21ba8291e10
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-09-01 13:39:25 +0300

    done task

--- script/script.js
+++ script/script.js
@@ -1,36 +1,22 @@
-const canvas = document.querySelector("#draw");
+const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
 
-const ctx = canvas.getContext("2d");
+let lastCahcked;
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-ctx.strokeStyle = "green";
-ctx.lineJoin = "round";
-ctx.lineCap = "round";
-
-let isDrawing = false;
-let X = 0;
-let Y = 0;
-const draw = e => {
-  if (!isDrawing) {
-    return;
+const handleChange = function(e) {
+  let inBeetwen = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastCahcked) {
+        inBeetwen = !inBeetwen;
+      }
+      if (inBeetwen) {
+        checkbox.checked = true;
+      }
+    });
   }
-  console.log(e.offsetX);
-  console.log(e.offsetY);
-  console.log(ctx);
-  ctx.beginPath();
-  //start from
-  // ctx.moveTo(X, Y);
-  //go to
-
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+  lastCahcked = this;
 };
 
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", e => {
-  isDrawing = true;
-  [x, z] = [e.offsetX, e.offsetY];
-});
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
+checkboxes.forEach(checkbox =>
+  checkbox.addEventListener("click", handleChange)
+);
